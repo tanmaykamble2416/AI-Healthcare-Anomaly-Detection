4661ba1541f02c63da3a6fdaa0595c55f1b3b0f0
Tanmay Kamble <tanmaykamble456@gmail.com>
2026-03-08 14:18:50 +0530

    Activity 2.2: Feature selection for anomaly detection

--- model/train_model.py
+++ model/train_model.py
@@ -1,25 +1,30 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-print("Starting Exploratory Data Analysis")
+print("\nStarting Feature Selection...\n")
 
+# Load dataset
 data = pd.read_csv("data/patient_vitals.csv")
 
-vital_features = [
+# Selected features for anomaly detection
+selected_features = [
     "Heart Rate",
-    "Oxygen Saturation",
+    "Respiratory Rate",
     "Body Temperature",
+    "Oxygen Saturation",
     "Systolic Blood Pressure",
-    "Diastolic Blood Pressure"
+    "Diastolic Blood Pressure",
+    "Derived_HRV",
+    "Derived_MAP"
 ]
 
-plt.figure(figsize=(12,8))
+# Create feature dataset
+feature_data = data[selected_features]
 
-for i, feature in enumerate(vital_features, 1):
-    plt.subplot(2,3,i)
-    sns.histplot(data[feature], bins=40, kde=True)
-    plt.title(feature)
+print("Selected Features:")
+print(selected_features)
 
-plt.tight_layout()
-plt.show()
+print("\nFeature Dataset Shape:")
+print(feature_data.shape)
+
+print("\nPreview of Selected Feature Data:")
+print(feature_data.head())
