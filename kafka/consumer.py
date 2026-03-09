bc5126c1703560fc22eadfa3d5c15e481b61b630
Tanmay Kamble <tanmaykamble456@gmail.com>
2026-03-09 09:55:22 +0530

    Milestone 4 Activity 4.2: Real-time patient-wise anomaly detection using Kafka and Autoencoder

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,16 +1,73 @@
 from kafka import KafkaConsumer
 import json
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+
+print("Loading trained model...")
+
+model = load_model("model/autoencoder_model.h5", compile=False)
+
+print("Model loaded successfully")
+print("Model input shape:", model.input_shape)
+
+selected_features = [
+    "Heart Rate",
+    "Respiratory Rate",
+    "Body Temperature",
+    "Oxygen Saturation",
+    "Systolic Blood Pressure",
+    "Diastolic Blood Pressure",
+    "Derived_HRV",
+    "Derived_MAP"
+]
+
+scaler = MinMaxScaler()
 
 consumer = KafkaConsumer(
     "patient_vitals",
     bootstrap_servers="localhost:9092",
     auto_offset_reset="earliest",
-    enable_auto_commit=True,
-    group_id="healthcare-monitor",
-    value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
 )
 
-print("Kafka Consumer started...")
+print("Kafka Consumer started... Monitoring patients\n")
+
+window_size = 10
+buffer = []
 
 for message in consumer:
-    print("Received:", message.value)
+
+    data = message.value
+
+    features = [data[f] for f in selected_features]
+
+    buffer.append(features)
+
+    if len(buffer) > window_size:
+        buffer.pop(0)
+
+    if len(buffer) < window_size:
+        continue
+
+    df = pd.DataFrame(buffer, columns=selected_features)
+
+    scaled = scaler.fit_transform(df)
+
+    X = scaled.reshape(1, -1)
+
+    reconstruction = model.predict(X)
+
+    error = np.mean((X - reconstruction) ** 2)
+
+    threshold = 0.075
+
+    if error > threshold:
+        status = "ANOMALY DETECTED"
+    else:
+        status = "Normal"
+
+    print("\nPatient Window Ending ID:", data["Patient ID"])
+    print("Anomaly Score:", error)
+    print("Status:", status)
--- kafka/producer.py
+++ kafka/producer.py
@@ -5,18 +5,14 @@ import time
 
 print("Starting Kafka Producer...")
 
-# Kafka producer setup
 producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-# Load dataset
 data = pd.read_csv("data/patient_vitals.csv")
 
-# Send rows one by one
 for _, row in data.iterrows():
-
     message = row.to_dict()
 
     producer.send("patient_vitals", value=message)
